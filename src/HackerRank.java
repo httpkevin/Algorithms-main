b209c8a4d45d8af5430d0447575a8d341407015b
Kevin <michaelkevinmendes@gmail.com>
2021-12-17 11:09:40 -0300

    HackerRank

--- src/HackerRank.java
+++ src/HackerRank.java
@@ -1,3 +1,23 @@
-public class HackerRank {
-    
+import java.util.Scanner;
+
+public class HackerRank{
+    public static void main(String[] args) {
+       
+       Scanner entrada=new Scanner(System.in);
+       int peca, peca2, quantidade, quantidade2;
+       double unitaria, unitaria2;
+       
+       peca=entrada.nextInt();
+       quantidade=entrada.nextInt();
+       unitaria=entrada.nextInt();
+
+       peca2=entrada.nextInt();
+       quantidade2=entrada.nextInt();
+       unitaria2=entrada.nextInt();
+              
+       double soma=(unitaria*quantidade)+(unitaria2*quantidade2); 
+        
+       System.out.printf("VALOR A PAGAR: R$ %.2f\n", soma);
+    }
 }
+
