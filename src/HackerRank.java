c6b3aa098df957abfe8bafc2a92aeeee972fff44
httpkevin <michaelkevinmendes@gmail.com>
2021-12-20 20:01:46 -0300

    git commit -m

--- src/HackerRank.java
+++ src/HackerRank.java
@@ -1,23 +1,16 @@
-import java.util.Scanner;
-
+//overload  
 public class HackerRank{
     public static void main(String[] args) {
-       
-       Scanner entrada=new Scanner(System.in);
-       int peca, peca2, quantidade, quantidade2;
-       double unitaria, unitaria2;
-       
-       peca=entrada.nextInt();
-       quantidade=entrada.nextInt();
-       unitaria=entrada.nextInt();
-
-       peca2=entrada.nextInt();
-       quantidade2=entrada.nextInt();
-       unitaria2=entrada.nextInt();
-              
-       double soma=(unitaria*quantidade)+(unitaria2*quantidade2); 
-        
-       System.out.printf("VALOR A PAGAR: R$ %.2f\n", soma);
+      int result = sum(5, 10);
+      System.out.printf("Testing");
+      System.out.println(result);
     }
-}
+    public static int sum(int start, int end) {
+      if (end > start) {
+        return end + sum(start, end - 1);
+      } else {
+        return end;
+      }
+    }
+  }
 
