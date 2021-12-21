76109f6128bae104ffcf60a7fbe3817826b99f00
httpkevin <michaelkevinmendes@gmail.com>
2021-12-20 21:08:39 -0300

    Beecrowd 1001 | Extremamente Básico

---
+++ src/Beecrowd1001.java
@@ -0,0 +1,13 @@
+//Beecrowd 1001 | Extramamente Básico
+import java.util.Scanner;
+
+public class Beecrowd1001{
+   
+public static void main(String[] Args) {
+        Scanner leia=new Scanner(System.in);
+        int A=leia.nextInt();
+        int B=leia.nextInt();
+        int X=A+B;
+        System.out.println("X = "+ X);
+    }
+}
