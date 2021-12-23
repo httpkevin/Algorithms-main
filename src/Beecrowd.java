782a81a3595f8e7e482fbe895a5fdea406e94289
httpkevin <michaelkevinmendes@gmail.com>
2021-12-22 22:03:54 -0300

    Beecrowd 1000  | hello World

--- src/Beecrowd.java
+++ src/Beecrowd.java
@@ -1,6 +1,7 @@
 //Beecrowd 1000 | Hello World!
 
-/*O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+/*
+O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
 
 Entrada
 Este problema não possui nenhuma entrada.
