184cef6cedc30254be8d4892a80c422e11d3fe75
httpkevin <michaelkevinmendes@gmail.com>
2021-12-22 21:59:14 -0300

    Beecrowd 1002  | Área do Círculo

--- src/Beecrowd1002.java
+++ src/Beecrowd1002.java
@@ -1,13 +1,21 @@
-// Beecrowd 1002 | Área do Círculo 
+// Beecrowd 1002 | Área do Círculo
+
+/* 
+A fórmula para calcular a área de uma circunferência é: area = π . raio2. Considerando para este problema que π = 3.14159:
+
+- Efetue o cálculo da área, elevando o valor de raio ao quadrado e multiplicando por π.
+
+Entrada
+A entrada contém um valor de ponto flutuante (dupla precisão), no caso, a variável raio.
+*/
 import java.util.Scanner; 
 
 public class Beecrowd1002 {
     public static void main(String[] Args){
         //area = n.raio -> area = 3.14159
-        Scanner intput=new Scanner(System.in);
-        double raio=intput.nextDouble(); 
-        double area=3.14159*(raio*2);
-        double calculation=3.14159*(Math.pow(raio,2));
-        System.out.printf("A=%.4f\n", calculation);
+        Scanner intput=new Scanner(System.in); //Utilizando função Scanner
+        double raio=intput.nextDouble(); //Declarando intput na variável raio 
+        double calculation=3.14159*(Math.pow(raio,2)); //Calculei utilizando a função Math.pow para elevar o raio ao quadrado 
+        System.out.printf("A=%.4f\n", calculation); //Output
     } 
 }
