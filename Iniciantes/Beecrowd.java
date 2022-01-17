104390eed5fdee2d25bd7f465349837f86e82b8b
httpkevin <michaelkevinmendes@gmail.com>
2022-01-17 20:40:35 -0300

    Beecrowd 1000 | Hello World

--- Iniciantes/Beecrowd.java
+++ Iniciantes/Beecrowd.java
@@ -9,6 +9,7 @@ Este problema não possui nenhuma entrada.
 Saída
 Você deve imprimir a mensagem "Hello World!" e em seguida o final de linha, conforme o exemplo abaixo.
 */
+
 public class Beecrowd {
         public static void main (String[] args){
             System.out.println("Hello World!");
