ff26784276c681b6e3ced78bb238e1bf09b1b10d
httpkevin <michaelkevinmendes@gmail.com>
2022-01-17 22:25:13 -0300

    Beecrowd 1013 | O maior

--- Iniciantes/Beecrowd1013.java
+++ Iniciantes/Beecrowd1013.java
@@ -1,4 +1,5 @@
 // Beecrowd 1012 | O maior 
+
 /*
 Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido da mensagem “eh o maior”. Utilize a fórmula:
 
@@ -13,19 +14,19 @@ O arquivo de entrada contém três valores inteiros.
 
 Saída
 Imprima o maior dos três valores seguido por um espaço e a mensagem "eh o maior".
-
-
 */
 
 import java.util.Scanner;
 
 public class Beecrowd1013 {
     public static void main(String[] args){ 
+        
         //Recebe os dados de entrada 
         Scanner input = new Scanner(System.in);
         int a = input.nextInt();
         int b = input.nextInt();
         int c = input.nextInt();
+        
         //Processa as condições
         if ( a > b && a > c) { // Se "a" for > que "b" e "c" então: "a" eh o maior
             System.out.println(a + " eh o maior"); //Retorno da condição
