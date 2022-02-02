30ea28c883c7817d24a16e2a67adc28b2517ed6a
Kevin <michaelkevinmendes@gmail.com>
2022-02-01 23:35:26 -0300

    Beecrowd 1028 | Figurinhas

--- Math/Beecrowd1028.java
+++ Math/Beecrowd1028.java
@@ -9,7 +9,46 @@ Saída
 Para cada caso de teste de entrada haverá um valor na saída, representando o tamanho máximo da pilha de figurinhas que poderia ser trocada entre dois jogadores.
 
 */
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class Beecrowd1028 {
-    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static int gcd(int a, int b){
+        return (a==0)? b : gcd(b % a, a);
+    }
+
+    public static int[] getNums(String line, int qtd){
+        StringBuilder bodybuilder = new StringBuilder();
+        int[] nums = new int[qtd];
+        int len = line.length(), cont = 0;
+        char c;
+        for(int i = 0; i < len; i++){
+            c = line.charAt(i);
+            if(c>=48 && c<=57){
+                bodybuilder.append(c);
+                if(i == len-1)
+                    nums[cont] = Integer.parseInt(bodybuilder.toString());
+            }
+            else{
+                nums[cont] = Integer.parseInt(bodybuilder.toString());
+                cont++;
+                bodybuilder.delete(0, bodybuilder.length());
+            }
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) throws IOException{
+        int n = Integer.parseInt(br.readLine());
+        int[] nums;
+        while(n-- > 0){
+            nums = getNums(br.readLine(), 2);
+            System.out.println(gcd(nums[0], nums[1]));
+        }
+    }
 }
